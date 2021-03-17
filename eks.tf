49a1832acaaf9b5e91452659b20faa958a1fc849
Luis Rascao <luis.rascao@miniclip.com>
2021-03-17 19:35:40 +0000

    Add multi autoscaling group capability
    Provide two different ASGs, one for on-demand and another for spot
    instances. Cluster autoscaler can then choose appropriately depending
    on the desired lifecycle of each pod workload.

--- eks.tf
+++ eks.tf
@@ -19,35 +19,69 @@ module "eks" {
     tags = var.tags
 
     worker_groups_launch_template = [
-    {
-      name                    = "spot-ng-1"
-      instance_type           = var.instance_types[0]
-      override_instance_types = var.instance_types
-      spot_instance_pools     = length(var.instance_types)
-      asg_min_size            = var.min_size 
-      asg_max_size            = var.max_size 
-      asg_desired_capacity    = 1
-      on_demand_base_capacity = var.on_demand_base_capacity 
-      on_demand_percentage_above_base_capacity = var.on_demand_percentage_above_base_capacity
-
-      kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=`curl -s http://169.254.169.254/latest/meta-data/instance-life-cycle`"
-      public_ip               = true
-      autoscalling_enabled    = true
-      pre_userdata            = var.userdata_prefix
-      additional_userdata     = var.userdata_suffix
-      tags = [
         {
-          "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}"
-          "propagate_at_launch" = "false"
-          "value"               = "owned"
+          name                    = "spot-ng-1"
+          instance_type           = var.instance_types[0]
+          override_instance_types = var.instance_types
+          spot_instance_pools     = length(var.instance_types)
+          asg_min_size            = var.min_spot_size 
+          asg_max_size            = var.max_spot_size 
+          asg_desired_capacity    = 0
+
+          kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=spot"
+          public_ip               = true
+          autoscalling_enabled    = true
+          pre_userdata            = var.userdata_prefix
+          additional_userdata     = var.userdata_suffix
+          tags = [
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}"
+              "propagate_at_launch" = "false"
+              "value"               = "owned"
+            },
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/enabled"
+              "propagate_at_launch" = "false"
+              "value"               = "true"
+            },
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/node-template/label/node.kubernetes.io/lifecycle"
+              "propagate_at_launch" = "true"
+              "value"               = "spot"
+            }
+          ]
         },
         {
-          "key"                 = "k8s.io/cluster-autoscaler/enabled"
-          "propagate_at_launch" = "false"
-          "value"               = "true"
+          name                    = "on-demand-ng-2"
+          instance_type           = var.instance_types[0]
+          asg_min_size            = var.min_on_demand_size 
+          asg_max_size            = var.max_on_demand_size 
+          asg_desired_capacity    = 0
+
+          kubelet_extra_args      = "--node-labels=node.kubernetes.io/lifecycle=on-demand"
+          public_ip               = true
+          autoscalling_enabled    = true
+          pre_userdata            = var.userdata_prefix
+          additional_userdata     = var.userdata_suffix
+          tags = [
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/${var.cluster_name}"
+              "propagate_at_launch" = "false"
+              "value"               = "owned"
+            },
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/enabled"
+              "propagate_at_launch" = "false"
+              "value"               = "true"
+            },
+            {
+              "key"                 = "k8s.io/cluster-autoscaler/node-template/label/node.kubernetes.io/lifecycle"
+              "propagate_at_launch" = "true"
+              "value"               = "on-demand"
+            }
+          ]
         }
-      ]
-    }]
+    ]
 
     // this allows the power user root access over the k8s cluster
     map_roles = [
@@ -77,18 +111,3 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
 
-// outputs
-output "cluster_id" {
-    value = module.eks.cluster_id
-    description = "k8s cluster id"
-}
-
-output "worker_node_iam_role_arn" {
-    value = module.eks.worker_iam_role_arn
-    description = "IAM Role ARN of the worker nodes"
-}
-
-output "worker_node_iam_role_name" {
-    value = module.eks.worker_iam_role_name
-    description = "IAM Role name of the worker nodes"
-}
